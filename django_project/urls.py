976f9239aa2fad4c20760cbebb83e66fe6646178
JuliooCesar-alt <juliotechti@gmail.com>
2024-12-15 13:37:04 +0000

    teste de atualização
    Testando a sincronização com git

--- django_project/urls.py
+++ django_project/urls.py
@@ -5,7 +5,7 @@ from calculadora import views
 urlpatterns = [
     path('', views.vazio, name='vazio'),# Página inicial
     path('vazio', views.home, name='home'),  
-    path('soma/', views.soma, name='soma'),                    path('divisao/', views.divisao, name='divisao'),
+    path('soma/', views.soma, name='soma'),                                            path('divisao/', views.divisao, name='divisao'),
     path('subtrair/', views.subtrair, name='subtrair'),
     path('multiplicar/', views.multiplicar, name='multiplicar'),  
     path('autor/', views.autor, name='autor'),
