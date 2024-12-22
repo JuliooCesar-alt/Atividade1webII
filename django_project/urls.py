4702f0e30fb24ccf2b9c78f0d5b83616343ec4b0
JuliooCesar-alt <juliotechti@gmail.com>
2024-12-22 15:30:14 +0000

    Salvando alterações antes de mudar para a branch main

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
 from django.urls import path
 from calculadora import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import AlunoViewSet
+
+router = DefaultRouter()
+router.register(r'alunos', AlunoViewSet)
 
 urlpatterns = [
-    path('', views.vazio, name='vazio'),# Página inicial
-    path('vazio', views.home, name='home'),  
-    path('soma/', views.soma, name='soma'),                                            path('divisao/', views.divisao, name='divisao'),
+    path('', views.index, name='index'),  # Página inicial
+    path('index/', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('soma/', views.soma, name='soma'),
+    path('divisao/', views.divisao, name='divisao'),
     path('subtrair/', views.subtrair, name='subtrair'),
-    path('multiplicar/', views.multiplicar, name='multiplicar'),  
+    path('multiplicar/', views.multiplicar, name='multiplicar'),
     path('autor/', views.autor, name='autor'),
-    ]
+    path('enquete/', views.enquete, name='enquete'),
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+]
