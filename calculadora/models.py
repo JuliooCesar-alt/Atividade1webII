49c86279690e8426b161745e8888c591626112bd
JuliooCesar-alt <juliotechti@gmail.com>
2024-12-22 15:45:52 +0000

    adicionando  classes ao models
    exemplo usando aluno e curso

--- calculadora/models.py
+++ calculadora/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Curso(models.Model):
+  nome = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.nome
+
+
+class Aluno(models.Model):
+  nome = models.CharField(max_length=100)
+  idade = models.IntegerField()
+  curso = models.ForeignKey(Curso,
+                            on_delete=models.CASCADE,
+                            related_name='alunos')
+
+  def __str__(self):
+    return self.nome
