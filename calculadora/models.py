fde38f99a00db9c8b0ad304d911f992f93e7defc
JuliooCesar-alt <juliotechti@gmail.com>
2024-12-22 15:59:21 +0000

    Correção para separar tarefa da unidade 3
    Houve uma bagunça, então decidir separar a unidade 3

--- calculadora/models.py
+++ calculadora/models.py
@@ -1,19 +1,3 @@
 from django.db import models
 
-
-class Curso(models.Model):
-  nome = models.CharField(max_length=100)
-
-  def __str__(self):
-    return self.nome
-
-
-class Aluno(models.Model):
-  nome = models.CharField(max_length=100)
-  idade = models.IntegerField()
-  curso = models.ForeignKey(Curso,
-                            on_delete=models.CASCADE,
-                            related_name='alunos')
-
-  def __str__(self):
-    return self.nome
+# Create your models here.
--- django_project/urls.py
+++ django_project/urls.py
@@ -2,14 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from calculadora import views
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import AlunoViewSet
-
-router = DefaultRouter()
-router.register(r'alunos', AlunoViewSet)
 
 urlpatterns = [
-
     path('', views.index, name='index'),  # Página inicial
     path('index/', views.index, name='index'),
     path('home/', views.home, name='home'),
@@ -19,7 +13,4 @@ urlpatterns = [
     path('multiplicar/', views.multiplicar, name='multiplicar'),
     path('autor/', views.autor, name='autor'),
     path('enquete/', views.enquete, name='enquete'),
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
 ]
-
